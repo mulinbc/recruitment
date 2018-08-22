1221c59db20aed94e1709ef52b9cd071e09ffada
MuLin <mulin@bbcclive.com>
2018-08-23 00:08:18 +0800

    Add solution 070 121

---
+++ Leetcode/src/main/java/com/bbcclive/leetcode/solution/_121/Solution.java
@@ -0,0 +1,24 @@
+package com.bbcclive.leetcode.solution._121;
+
+public class Solution {
+    public int maxProfit(int[] prices) {
+        int ret = 0, minPrice = Integer.MAX_VALUE;
+        for (int price : prices) {
+            if (price < minPrice)
+                minPrice = price;
+            ret = Math.max(ret, price - minPrice);
+        }
+        return ret;
+    }
+
+    // 暴力法
+    // public int maxProfit(int[] prices) {
+    // int ret = 0;
+    // for (int i = 0; i < prices.length; i++) {
+    // for (int j = i + 1; j < prices.length; j++) {
+    // ret = Math.max(ret, prices[j] - prices[i]);
+    // }
+    // }
+    // return ret;
+    // }
+}
---
+++ Leetcode/src/test/java/com/bbcclive/leetcode/solution/_070/SolutionTest.java
@@ -0,0 +1,15 @@
+package com.bbcclive.leetcode.solution._070;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class SolutionTest {
+    @Test
+    public void testLengthOfLongestSubstring() {
+        int n = 3;
+        int expected = 3;
+        int actual = new Solution().climbStairs(n);
+        assertEquals(expected, actual);
+    }
+}
