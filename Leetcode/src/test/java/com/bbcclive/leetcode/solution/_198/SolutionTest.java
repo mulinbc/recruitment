0393f2c005b37c23ca7475431dfb5c1da3118cec
MuLin <mulin@bbcclive.com>
2018-08-24 14:59:51 +0800

    Add solution 198

---
+++ Leetcode/src/test/java/com/bbcclive/leetcode/solution/_198/SolutionTest.java
@@ -0,0 +1,15 @@
+package com.bbcclive.leetcode.solution._198;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class SolutionTest {
+    @Test
+    public void testLengthOfLongestSubstring() {
+        int[] nums = { 1, 2, 3, 1 };
+        int expected = 4;
+        int actual = new Solution().rob(nums);
+        assertEquals(expected, actual);
+    }
+}
