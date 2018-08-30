102515c8c100b6fbce4c4b2a7f153c8cd987f89f
MuLin <mulin@bbcclive.com>
2018-08-30 17:50:41 +0800

    Add exam example

---
+++ Exam/src/main/java/exam/_1/Main.java
@@ -0,0 +1,24 @@
+package exam._1;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        Solution solution = new Solution();
+        System.out.println(solution.add(a, b));
+        
+        sc.close();
+    }
+}
+
+class Solution {
+    public int add(int a, int b) {
+        int ret = 0;
+        ret = a + b;
+        return ret;
+    }
+}
---
+++ Exam/src/test/java/exam/_3/MainTest.java
@@ -0,0 +1,15 @@
+package exam._3;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class MainTest {
+    @Test
+    public void testAdd() {
+        int a = 1, b = 2;
+        int expected = 3;
+
+        int actual = new Solution().add(a, b);
+        assertEquals(expected, actual);
+    }
+}
