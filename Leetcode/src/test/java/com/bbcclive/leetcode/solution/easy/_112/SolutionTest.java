e2fd95a82cdb6978c402a558ae9b7547025d82f1
MuLin <mulin@bbcclive.com>
2018-09-08 11:01:48 +0800

    Add solution 113 and modify solution 112

--- Leetcode/src/test/java/com/bbcclive/leetcode/solution/easy/_112/SolutionTest.java
+++ Leetcode/src/test/java/com/bbcclive/leetcode/solution/easy/_112/SolutionTest.java
@@ -9,10 +9,11 @@ import org.junit.Test;
 public class SolutionTest {
     @Test
     public void testLengthOfLongestSubstring() {
-        String strTree="5 4 8 11 null 13 4 7 2 null null null 1";
-        TreeNode root=TreeNode.createTree(strTree);
+        String strTree = "5 4 8 11 null 13 4 7 2 null null null 1";
+        int sum = 22;
+        TreeNode root = TreeNode.createTree(strTree);
         boolean expected = true;
-        boolean actual = new Solution().hasPathSum(root, 22);
+        boolean actual = new Solution().hasPathSum(root, sum);
         assertEquals(expected, actual);
     }
 }
